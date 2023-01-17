e72e50f8a53a455c0cb2118f96b9343d9cbd6bf5
nikhil98051 <106965666+nikhil98051@users.noreply.github.com>
2023-01-17 09:17:00 +0530

    Add files via upload

--- exceptionmismatch.java
+++ exceptionmismatch.java
@@ -1,27 +1,35 @@
-
-    import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-public class exceptionmismatch{
-
-
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc = new Scanner(System.in);
-        try{
-        try{
-            int x = (sc.nextInt());
-            int y = 12(sc.nextInt());
-            System.out.println(""+(x/y));
-        }catch (InputMismatchException e){
-            System.out.println("java.util.InputMismatchException");
+ class InvalidMismatchException extends Exception{
+ public InvalidMismatchException(String str){
+    super(str);
+ }
+}
+public class signed{
+    static void is32BitSignedInteger(int num)throws InvalidMismatchException {
+      if((num <= 2147483647) && (num >= -2147483648))
+      {
+         System.out.println("Signed");
+       }
+     else {
+        throw new InvalidMismatchException("Invalid");
         }
-            
-        }catch(Exception e){
+    }
+    public static void main(String a[]){
+        Scanner ob=new Scanner(System.in);
+        try{
+            int x=ob.nextInt();
+            int y=ob.nextInt();
+            is32BitSignedInteger(x);
+            is32BitSignedInteger(y);
+            int c=x/y;
+            System.out.println(c);
+      
+        }
+        catch(ArithmeticException e){
             System.out.println(e);
         }
-        
+        catch(InvalidMismatchException e){
+            System.out.println(e);
+        }
     }
+     
 }
